35fda3e3bd5f4906f483456dc0fa8376c6d9d3ee
Jared Buckner <buckner.jared@gmail.com>
2017-01-28 20:21:27 -0600

    get first option to animate/load

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -16,41 +16,44 @@ function resize() {
 }
 
 function start() {
-  
-  // loadAnimation();
   loadIntroTextOne();
 }
 
 function loadIntroTextOne() {
-  var text = new createjs.Text("let's go on an ", "50px quicksandregular", "white");
+  let xOffset = 75;
+  let yOffset = 100;
+
+  let text = new createjs.Text("let's go on an ", "50px quicksandregular", "white");
   text.alpha = 0;
-  text.x = (stage.canvas.width / 2) - text.getMeasuredWidth();
-  text.y = (stage.canvas.height / 2) - 100;
+  text.x = (stage.canvas.width / 2) - text.getMeasuredWidth() - xOffset;
+  text.y = (stage.canvas.height / 2) - yOffset;
   stage.addChild(text);
 
-  var textAdventure = new createjs.Text("adventure ", "50px quicksandregular", "#D5A217");
+  let textAdventure = new createjs.Text("adventure ", "50px quicksandregular", "#D5A217");
   textAdventure.alpha = 0;
-  textAdventure.x = (stage.canvas.width / 2);
-  textAdventure.y = (stage.canvas.height / 2) - 100;
+  textAdventure.x = (stage.canvas.width / 2) - xOffset;
+  textAdventure.y = (stage.canvas.height / 2) - yOffset;
   stage.addChild(textAdventure);
 
-  var textTogether = new createjs.Text("together", "50px quicksandregular", "white");
+  let textTogether = new createjs.Text("together", "50px quicksandregular", "white");
   textTogether.alpha = 0;
-  textTogether.x = (stage.canvas.width / 2) + textAdventure.getMeasuredWidth();
-  textTogether.y = (stage.canvas.height / 2) - 100;
+  textTogether.x = (stage.canvas.width / 2) + textAdventure.getMeasuredWidth() - xOffset;
+  textTogether.y = (stage.canvas.height / 2) - yOffset;
   stage.addChild(textTogether);
+  console.log(textAdventure.getMeasuredWidth());
+  console.log(textTogether.getMeasuredWidth());
 
   createjs.Tween.get(text, { loop: false })
-  .to({ alpha: 1 }, 3500, createjs.Ease.getPowIn(1.5))
-  .to({ alpha: 0 }, 3500, createjs.Ease.getPowIn(1.5));
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .to({ alpha: 0 }, 4000, createjs.Ease.getPowIn(2));
 
   createjs.Tween.get(textAdventure, { loop: false })
-  .to({ alpha: 1 }, 3500, createjs.Ease.getPowIn(1.5))
-  .to({ alpha: 0 }, 3500, createjs.Ease.getPowIn(1.5));
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .to({ alpha: 0 }, 4000, createjs.Ease.getPowIn(2));
 
   createjs.Tween.get(textTogether, { loop: false })
-  .to({ alpha: 1 }, 3500, createjs.Ease.getPowIn(1.5))
-  .to({ alpha: 0 }, 3500, createjs.Ease.getPowIn(1.5))
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .to({ alpha: 0 }, 4000, createjs.Ease.getPowIn(2))
   .call(loadIntroTextTwo);
 
   createjs.Ticker.setFPS(60);
@@ -58,25 +61,28 @@ function loadIntroTextOne() {
 }
 
 function loadIntroTextTwo() {
-  var text = new createjs.Text("let's go somewhere ", "50px quicksandregular", "white");
+  let xOffset = 125;
+  let yOffset = 100;
+
+  let text = new createjs.Text("let's go somewhere ", "50px quicksandregular", "white");
   text.alpha = 0;
-  text.x = (stage.canvas.width / 2) - text.getMeasuredWidth();
-  text.y = (stage.canvas.height / 2) - 100;
+  text.x = (stage.canvas.width / 2) - text.getMeasuredWidth() + xOffset;
+  text.y = (stage.canvas.height / 2) - yOffset;
   stage.addChild(text);
 
-  var textFarAway = new createjs.Text("far away", "50px quicksandregular", "#D5A217");
+  let textFarAway = new createjs.Text("far away", "50px quicksandregular", "#D5A217");
   textFarAway.alpha = 0;
-  textFarAway.x = (stage.canvas.width / 2);
-  textFarAway.y = (stage.canvas.height / 2) - 100;
+  textFarAway.x = (stage.canvas.width / 2) + xOffset;
+  textFarAway.y = (stage.canvas.height / 2) - yOffset;
   stage.addChild(textFarAway);
 
   createjs.Tween.get(text, { loop: false })
-  .to({ alpha: 1 }, 3500, createjs.Ease.getPowIn(1.5))
-  .to({ alpha: 0 }, 3500, createjs.Ease.getPowIn(1.5));
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .to({ alpha: 0 }, 4000, createjs.Ease.getPowIn(2));
 
   createjs.Tween.get(textFarAway, { loop: false })
-  .to({ alpha: 1 }, 3500, createjs.Ease.getPowIn(1.5))
-  .to({ alpha: 0 }, 3500, createjs.Ease.getPowIn(1.5))
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .to({ alpha: 0 }, 4000, createjs.Ease.getPowIn(2))
   .call(loadOptions);
 
   createjs.Ticker.setFPS(60);
@@ -84,19 +90,34 @@ function loadIntroTextTwo() {
 }
 
 function loadOptions() {
-  loadAnimation();
+  let text = new createjs.Text("where shall we go?", "50px quicksandregular", "white");
+  text.alpha = 0;
+  text.x = (stage.canvas.width / 2) - (text.getMeasuredWidth() / 2);
+  text.y = 50;
+  stage.addChild(text);
+
+  createjs.Tween.get(text, { loop: false })
+  .to({ alpha: 1 }, 4000, createjs.Ease.getPowIn(2))
+  .call(loadAnimation);
 }
 
 
 function loadAnimation() {
   forestRoot = new lib.ForestAnimation();
-  console.log(forestRoot);
-  forestRoot.x = 300;
-  forestRoot.y = 300;
-  stage.addChild(forestRoot); 
+  forestRoot.x = 100;
+  forestRoot.y = 200;
+  stage.addChild(forestRoot);
 
-  treeRoot = new lib.RoundTreeGrowing();
-  stage.addChild(treeRoot);
+  console.log(forestRoot);
+
+  let text = new createjs.Text("the forest", "30px quicksandregular", "white");
+  text.alpha = 0;
+  text.x = forestRoot.x + text.getMeasuredWidth() + 55;
+  text.y = forestRoot.y + 360;
+  stage.addChild(text);
+
+  createjs.Tween.get(text, { loop: false })
+  .to({ alpha: 1 }, 3000, createjs.Ease.getPowIn(2))
 
   createjs.Ticker.setFPS(24);
   createjs.Ticker.addEventListener("tick", stage);
